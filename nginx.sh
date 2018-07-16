b8ba1c159894771d24c6c93cc4fee0b8d3999610
Praveen Premaratne <praveen.mayantha@gmail.com>
2018-07-16 14:01:40 +0100

    Nginx installation script added

--- nginx.sh
+++ nginx.sh
@@ -1,1 +1,10 @@
 
+export LANGUAGE=en_UK.UTF-8
+export LANG=en_UK.UTF-8
+export LC_ALL=en_UK.UTF-8
+locale-gen en_UK.UTF-8
+dpkg-reconfigure locales
+apt-get -y update
+apt-get -y upgrade
+
+apt-get -y install curl vim nginx
