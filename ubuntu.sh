660210c4b32281c67604870f589708c332f1662c
Praveen Premaratne <praveen.mayantha@gmail.com>
2018-09-02 21:16:09 +0100

    exports removed and code rearranged

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,10 +1,4 @@
-
+dpkg-reconfigure locales
 apt-get -y update
 apt-get -y upgrade
-export LANGUAGE=en_UK.UTF-8
-export LANG=en_UK.UTF-8
-export LC_ALL=en_UK.UTF-8
-locale-gen en_UK.UTF-8
-dpkg-reconfigure locales
-
 apt-get -y install vim
