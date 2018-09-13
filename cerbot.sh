d54234c180659a62a59c42dfa6055c6d5a0a32f9
Praveen Premaratne <praveen.mayantha@gmail.com>
2018-09-13 12:31:56 +0100

    Missing auto accept added

--- cerbot.sh
+++ cerbot.sh
@@ -1,5 +1,5 @@
 apt-get update -y
-apt-get install software-properties-common
+apt-get install software-properties-common -y
 add-apt-repository ppa:certbot/certbot -y
 apt-get update -y
 apt-get install python-certbot-nginx
