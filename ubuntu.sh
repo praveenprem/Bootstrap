5ad1fbaeb61d46f1b90ab289073d850c0c75794e
Praveen Premaratne <praveen.premaratne@groupcdg.com>
2019-01-13 17:42:46 +0000

    Security improved

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,3 +2,13 @@ dpkg-reconfigure locales
 apt-get -y update
 apt-get -y upgrade
 apt-get -y install vim
+
+# OpenSSH Upgrade
+wget https://raw.githubusercontent.com/praveenprem/Bootstrap/master/resources/sshd_config -O /tmp/sshd_config
+mv /tmp/sshd_config /etc/ssh/sshd_config
+service ssh restart
+
+# Network security
+ufw allow 22/tcp
+ufw allow OpenSSH
+ufw --force enable
