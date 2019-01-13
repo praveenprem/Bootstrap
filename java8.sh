7a98c563ebd59fde754409ce3f3aefc03deb76d6
Praveen Premaratne <praveen.premaratne@groupcdg.com>
2019-01-13 20:10:50 +0000

    Java8 Installation script

---
+++ java8.sh
@@ -0,0 +1,13 @@
+apt-get -y update
+apt-get -y upgrade
+add-apt-repository -y ppa:webupd8team/java
+apt-get -y update
+echo 'oracle-java8-installer shared/accepted-oracle-license-v1-1 boolean true' | debconf-set-selections
+DEBIAN_FRONTEND=noninteractive apt-get -y install oracle-java8-installer
+
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle/
+export PATH=$JAVA_HOME/bin:$PATH
+
+echo "export JAVA_HOME=/usr/lib/jvm/java-8-oracle/" >> ~/.bashrc
+echo "export PATH=$JAVA_HOME/bin:$PATH" >> ~/.bashrc
+source ~/.bashrc
