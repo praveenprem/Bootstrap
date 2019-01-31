e2e9ac4b2f7baf31af4eb1a18e25ff6c8cba92b1
Praveen Premaratne <praveen.premaratne@groupcdg.com>
2019-01-31 21:32:16 +0000

    SSH config updated and SSHAUTH plugin added

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,12 +1,17 @@
-dpkg-reconfigure locales
 apt-get -y update
 apt-get -y upgrade
-apt-get -y install vim
+apt-get -y install vim ufw wget
 
 # OpenSSH Upgrade
 wget https://raw.githubusercontent.com/praveenprem/Bootstrap/master/resources/sshd_config -O /tmp/sshd_config
 mv /tmp/sshd_config /etc/ssh/sshd_config
 service ssh restart
+
+wget https://github.com/praveenprem/sshauth/releases/download/v1.1.0/sshauth-1.1.0-binary.tar.gz -O- | tar -xzvf -
+mv sshauth /usr/local/bin/
+chmod +x /usr/local/bin/sshauth
+mkdir -p /etc/sshauth/
+mv config.yml /etc/sshauth/
 
 # Network security
 ufw allow 22/tcp
