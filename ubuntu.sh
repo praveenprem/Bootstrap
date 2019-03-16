917e662d4583673681ed05152c0563f357268470
Praveen Premaratne <praveen.premaratne@groupcdg.com>
2019-03-16 17:43:08 +0000

    Minimum system update/upgrade and software installation added

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,19 +1,7 @@
 apt-get -y update
 apt-get -y upgrade
-apt-get -y install vim ufw wget
 
-# OpenSSH Upgrade
-wget https://raw.githubusercontent.com/praveenprem/Bootstrap/master/resources/sshd_config -O /tmp/sshd_config
-mv /tmp/sshd_config /etc/ssh/sshd_config
-service ssh restart
+apt-get install software-properties-common -y
+apt-get -y update
 
-wget https://github.com/praveenprem/sshauth/releases/download/v1.1.0/sshauth-1.1.0-binary.tar.gz -O- | tar -xzvf -
-mv sshauth /usr/local/bin/
-chmod +x /usr/local/bin/sshauth
-mkdir -p /etc/sshauth/
-mv config.yml /etc/sshauth/
-
-# Network security
-ufw allow 22/tcp
-ufw allow OpenSSH
-ufw --force enable
+apt-get -y install vim wget
