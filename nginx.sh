37ce946a9c9cc1a06cb8126c0cac64ee1c5be7a7
Praveen Premaratne <praveen.mayantha@gmail.com>
2022-07-16 13:41:09 +0100

    All more logging to Nginx installer

--- nginx.sh
+++ nginx.sh
@@ -12,6 +12,8 @@ apt-get -y update
 apt-get -y upgrade
 apt-get -y install nginx
 
+logger "Nginx installation completed"
+
 IFS= read -p "Do you want to install Certbot?: (Only accept yes) " install_certbot
 if [[ \$install_certbot == "yes" ]]; then
     wget https://raw.githubusercontent.com/praveenprem/Bootstrap/master/cerbot.sh -O /tmp/certbot.sh 
