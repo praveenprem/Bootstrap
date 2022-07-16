c20d7014c399285735d04cca54224f8659827f92
Praveen Premaratne <praveen.mayantha@gmail.com>
2022-07-16 13:01:56 +0100

    Update Certbot installation to add Cloudflare DNS plugin

--- cerbot.sh
+++ cerbot.sh
@@ -1,7 +1,26 @@
-wget https://raw.githubusercontent.com/praveenprem/Bootstrap/master/ubuntu.sh -O - | bash
+logger() {
+    echo "$(date '+%Y/%m/%d %H:%M:%S') certbot.sh: $1"
+}
 
-add-apt-repository ppa:certbot/certbot -y
+logger "Installing Certbot"
 
-apt-get update -y
+apt update; apt -y upgrade
 
-apt-get install python-certbot-nginx -y
+apt install certbot
+
+IFS= read -p "Do you want to install Cloudflare DNS plugin: (Only accept yes) " install_cf
+if [[ $install_cf == "yes" ]]; then
+    logger "Installing certbot-dns-cloudflare DNS challange automation"
+    apt install python3-pip -y
+    pip install certbot-dns-cloudflare
+
+    IFS= read -p "Please enter the Cloudflare access token: " cf_token
+    if [[ $cf_token == "" ]]; then
+        logger "Failed to configure Cloudflare DNS challange. No token"
+    else
+        logger "Saving credentials to ~/.secrets/certbot/cloudflare.ini"
+        mkdir -p ~/.secrets/certbot/
+        echo $cf_token > ~/.secrets/certbot/cloudflare.ini
+        chmod 600 ~/.secrets/certbot/cloudflare.ini
+    fi
+fi
