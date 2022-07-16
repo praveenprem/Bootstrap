e9c1da116ad899b7cc23cb9d116ab6dbf141d964
Praveen Premaratne <praveen.mayantha@gmail.com>
2022-07-16 13:48:43 +0100

    Refactor nginx intaller

--- nginx.sh
+++ nginx.sh
@@ -1,10 +1,8 @@
-cat <<EOF > /tmp/nginx.sh
 logger() {
-    echo "\$(date '+%Y/%m/%d %H:%M:%S') nginx.sh: \$1"
+    echo "$(date '+%Y/%m/%d %H:%M:%S') nginx.sh: $1"
 }
 
-wget https://raw.githubusercontent.com/praveenprem/Bootstrap/master/ubuntu.sh -O /tmp/ubuntu.sh
-bash /tmp/ubuntu.sh
+bash <(wget -O - https://raw.githubusercontent.com/praveenprem/Bootstrap/master/ubuntu.sh)
 
 logger "Installing Nginx"
 
@@ -15,9 +13,6 @@ apt-get -y install nginx
 logger "Nginx installation completed"
 
 IFS= read -p "Do you want to install Certbot?: (Only accept yes) " install_certbot
-if [[ \$install_certbot == "yes" ]]; then
-    wget https://raw.githubusercontent.com/praveenprem/Bootstrap/master/cerbot.sh -O /tmp/certbot.sh 
-    bash /tmp/certbot.sh
+if [[ $install_certbot == "yes" ]]; then
+    bash <(wget -O -  https://raw.githubusercontent.com/praveenprem/Bootstrap/master/cerbot.sh)
 fi
-EOF
-bash /tmp/nginx.sh
