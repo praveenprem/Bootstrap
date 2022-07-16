52a1359c684c13c39fe247daecf5619de151d6ea
Praveen Premaratne <praveen.mayantha@gmail.com>
2022-07-16 13:02:42 +0100

    Add logging to Nginx instllare

--- nginx.sh
+++ nginx.sh
@@ -1,10 +1,16 @@
-
+logger() {
+    echo "$(date '+%Y/%m/%d %H:%M:%S') certbot.sh: $1"
+}
 
 wget https://raw.githubusercontent.com/praveenprem/Bootstrap/master/ubuntu.sh -O - | bash
 
-wget https://raw.githubusercontent.com/praveenprem/Bootstrap/master/cerbot.sh -O - | bash
+logger "Installing Nginx"
 
 apt-get -y update
 apt-get -y upgrade
+apt-get -y install nginx
 
-apt-get -y install curl vim nginx
+IFS= read -p "Do you want to install Certbot?: (Only accept yes) " install_certbot
+if [[ $install_certbot == "yes" ]]; then
+    wget https://raw.githubusercontent.com/praveenprem/Bootstrap/master/cerbot.sh -O - | bash
+fi
