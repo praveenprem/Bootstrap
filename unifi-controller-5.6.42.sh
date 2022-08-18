9ab97223f186279fec7ddfe64771f11a8f2030f3
Praveen Premaratne <23165760+praveenprem@users.noreply.github.com>
2022-08-18 16:04:36 +0100

    Add Unifi controller v5.6.42 installation

---
+++ unifi-controller-5.6.42.sh
@@ -0,0 +1,25 @@
+echo ' __  __     __   __     __     ______   __        ______     ______     __   __     ______   ______     ______     __         __         ______     ______    '
+echo '/\ \/\ \   /\ "-.\ \   /\ \   /\  ___\ /\ \      /\  ___\   /\  __ \   /\ "-.\ \   /\__  _\ /\  == \   /\  __ \   /\ \       /\ \       /\  ___\   /\  == \   '
+echo '\ \ \_\ \  \ \ \-.  \  \ \ \  \ \  __\ \ \ \     \ \ \____  \ \ \/\ \  \ \ \-.  \  \/_/\ \/ \ \  __<   \ \ \/\ \  \ \ \____  \ \ \____  \ \  __\   \ \  __<   '
+echo ' \ \_____\  \ \_\\"\_\  \ \_\  \ \_\    \ \_\     \ \_____\  \ \_____\  \ \_\\"\_\    \ \_\  \ \_\ \_\  \ \_____\  \ \_____\  \ \_____\  \ \_____\  \ \_\ \_\ '
+echo '  \/_____/   \/_/ \/_/   \/_/   \/_/     \/_/      \/_____/   \/_____/   \/_/ \/_/     \/_/   \/_/ /_/   \/_____/   \/_____/   \/_____/   \/_____/   \/_/ /_/ '
+echo '                                                                                                                                                              '
+
+echo "Version: 5.6.42"
+
+UNIFI_BRANCH="unifi-5.6"
+
+echo "**** add mongo repository ****"
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" >> /etc/apt/sources.list.d/mongo.list
+
+echo "**** install packages ****"
+apt-get update
+apt-get install -y binutils jsvc mongodb-org-server	openjdk-8-jre-headless wget curl
+
+echo "**** install unifi ****"
+UNIFI_VERSION=$(curl -sX GET "http://dl-origin.ubnt.com/unifi/debian/dists/${UNIFI_BRANCH}/ubiquiti/binary-amd64/Packages" | sed -n 's/^Version\:\ \(.*\)$/\1/p'); \
+curl -o unifi.deb -L "http://dl.ubnt.com/unifi/${UNIFI_VERSION}/unifi_sysvinit_all.deb"
+dpkg -i unifi.deb
+
+echo "Completed"
