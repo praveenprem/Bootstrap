6e3718868fb85799be61b93793845266901b14e2
Praveen Premaratne <23165760+praveenprem@users.noreply.github.com>
2022-08-18 16:16:04 +0100

    Add gnupg installation

--- unifi-controller-5.6.42.sh
+++ unifi-controller-5.6.42.sh
@@ -9,6 +9,9 @@ echo "Version: 5.6.42"
 
 UNIFI_BRANCH="unifi-5.6"
 
+apt update && apt upgrade -y
+apt install gnupg
+
 echo "**** add mongo repository ****"
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" >> /etc/apt/sources.list.d/mongo.list
