fbc5dc6693fcaa8ff6c5d8a1615df18516342e90
Praveen Premaratne <praveen.mayantha@gmail.com>
2022-11-17 20:40:55 +0000

    Update Omada install to only run on Xenial

--- omada_sdn-controller.sh
+++ omada_sdn-controller.sh
@@ -11,16 +11,24 @@ echo '                                                                          
 
 echo "Version: 5.6.3"
 
-apt update && apt upgrade -y
-apt install gnupg -y
+source /etc/os-release
+
+if [ "$VERSION_CODENAME" != "xenial" ]; then
+    echo "MongoDB version is NOT supported on $VERSION_CODENAME"
+    exit 1
+fi
+
+apt-get update && apt-get upgrade -y
+
+echo "**** Install packages ****"
+apt-get install -y binutils jsvc openjdk-8-jre-headless wget curl gnupg
 
 echo "**** Add mongo repository ****"
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
-echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" >> /etc/apt/sources.list.d/mongo.list
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu $VERSION_CODENAME/mongodb-org/3.4 multiverse" >> /etc/apt/sources.list.d/mongo.list
 
-echo "**** Install packages ****"
-apt-get update
-apt-get install -y binutils jsvc mongodb-org-server	openjdk-8-jre-headless wget curl
+echo "**** Install MongoDB ****"
+apt-get install -y mongodb-org-server
 
 echo "**** Install Omada SDN Controller ****"
 curl -o Omada_SDN_Controller_v5.6.3_Linux_x64.deb -L "https://static.tp-link.com/upload/software/2022/202210/20221024/Omada_SDN_Controller_v5.6.3_Linux_x64.deb"
